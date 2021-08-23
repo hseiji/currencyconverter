f4f8ef6a9e557505677366fa7db1079202371088
hseiji <henrique.hst@gmail.com>
2021-08-23 08:09:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+// import Button from 'react-bootstrap/Button';
+import { Button, Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react'
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'https://api.frankfurter.app/latest';
 
 function App() {
+  
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Alert variant="primary">This a Button</Alert> */}
+      {/* <Button>Test</Button> */}
+      <h1>Currency Converter</h1>
+      <CurrencyRow/>
+      <div>=</div>
+      <CurrencyRow/>
     </div>
   );
 }
